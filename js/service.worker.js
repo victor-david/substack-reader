6c5b36886cb4c7d8ff70a6c287ac8b7684961a56
Victor D. Sandiego <codepuppy@balticsoap.com>
2024-10-16 21:35:31 -0600

    Update service.worker.js
    Code cleanup

--- js/service.worker.js
+++ js/service.worker.js
@@ -1,16 +1,8 @@
 import ContentScript from "./content.js";
 import Updater from "./update.js";
 import Rules from "./rules.js";
-import Storage from "./storage.js"
-import Option from "./option.js";
 
 console.log("Service worker started");
-// Storage.getAsync().then((data) => console.log("Service Worker:", data));
-// Option.getSitesAsync().then((sites) => console.log("Service worker", sites));
-
-// const sites = Option.getSites();
-// console.log("Service Worker (sync sites)", sites);
-// console.log("Service Worker (sync all)", Storage.get());
 
 chrome.runtime.onInstalled.addListener(async (e) =>
 {
@@ -39,5 +31,3 @@ chrome.management.onEnabled.addListener(async ()=>
     await Updater.updateFromStorageAsync();
     await Rules.setContentRulesAsync();
 });
-
-// ContentScript.registerAsync();
